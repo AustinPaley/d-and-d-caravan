93d578060519fd18b8f701b2134daa9820d1496d
AustinPaley <austinpaley32@gmail.com>
2019-08-01 01:37:00 -0400

    Slight Upgrade to Character Show Controller - Still Need Serializers

--- caravan-backend/app/controllers/characters_controller.rb
+++ caravan-backend/app/controllers/characters_controller.rb
@@ -1,11 +1,11 @@
 class CharactersController < ApplicationController
   def index
     @characters = Character.all
-    render json: @characters
+    render json: @characters, except: [:created_at, :updated_at]
   end
 
   def show
     @character = Character.find(params[:id])
-    render json: @character, except: [:created_at, :updated_at, :race_id], include: :race
+    render json: {name: @character.name, race: @character.race.race, shop: @character.shop.name}
   end
 end
