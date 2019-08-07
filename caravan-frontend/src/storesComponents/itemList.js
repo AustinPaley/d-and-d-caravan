6c0e1797d2e9bdb9b6a1b0fbec91e5a44bf55953
AustinPaley <austinpaley32@gmail.com>
2019-08-06 23:09:53 -0400

    First Draft of Item List frontend Complete

--- caravan-frontend/src/storesComponents/itemList.js
+++ caravan-frontend/src/storesComponents/itemList.js
@@ -8,23 +8,30 @@ class ItemList extends React.Component{
   render(){
     return(
       <div>
-        <table>
-          <tbody>
-            <tr>
-              <th>Item Name</th><th>Item Type</th><th>Cost</th><th>Stock</th>
-            </tr>
-            {this.props.items.sort((a,b) => a.cost-b.cost).map(item => {
-              return (
-                <Fragment key={`item-number-${item.id}`}>
-                <tr>
-                  <td>{item.name}</td><td>{item.equipment_category}</td><td>{item.cost}</td><td>{item.current_stock}</td>
-                </tr>
-                </Fragment>
-              )
-            })
-            }
-          </tbody>
-        </table>
+        <div className="parchmentTop">
+        <div className="parchment"></div>
+        <div className="parchmentBody">
+          <table className="itemListTable">
+            <thead>
+              <tr>
+                <th className="sticky-header">Item Name</th><th className="sticky-header">Item Type</th><th className="sticky-header">Cost</th><th className="sticky-header">Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.items.sort((a,b) => a.cost-b.cost).map(item => {
+                return (
+                  <Fragment key={`item-number-${item.id}`}>
+                  <tr>
+                    <td>{item.name}</td><td>{item.equipment_category}</td><td>{item.cost}</td><td>{item.current_stock}</td>
+                  </tr>
+                  </Fragment>
+                )
+              })
+              }
+            </tbody>
+          </table>
+        </div>
+        <div className="parchmentBottom"></div></div>
       </div>
     )
   }
