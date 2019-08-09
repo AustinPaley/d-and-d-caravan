0da97124213d940ff1d9675d4411e2b0edd565c2
AustinPaley <austinpaley32@gmail.com>
2019-08-09 00:09:10 -0400

    Added add/remove icons to store lists

--- caravan-frontend/src/storesComponents/itemList.js
+++ caravan-frontend/src/storesComponents/itemList.js
@@ -1,4 +1,6 @@
 import React, {Fragment} from 'react';
+import PlusImage from '../images/plus-square-regular.svg';
+import MinusImage from '../images/minus-square-regular.svg';
 
 class ItemList extends React.Component{
 
@@ -14,7 +16,7 @@ class ItemList extends React.Component{
           <table className="itemListTable">
             <thead>
               <tr>
-                <th className="sticky-header">Item Name</th><th className="sticky-header">Item Type</th><th className="sticky-header">Cost</th><th className="sticky-header">Stock</th>
+                <th className="sticky-header">Item Name</th><th className="sticky-header">Item Type</th><th className="sticky-header">Cost</th><th className="sticky-header">Stock</th><th className="sticky-header">Add/Remove</th>
               </tr>
             </thead>
             <tbody>
@@ -22,7 +24,7 @@ class ItemList extends React.Component{
                 return (
                   <Fragment key={`item-number-${item.id}`}>
                   <tr>
-                    <td>{item.name}</td><td>{item.equipment_category}</td><td>{item.cost}</td><td>{item.current_stock}</td>
+                    <td>{item.name}</td><td>{item.equipment_category}</td><td>{item.cost}</td><td>{item.current_stock}</td><td><img src={PlusImage} className="add-remove-icons" alt="plusIcon"/><img className="add-remove-icons" src={MinusImage} alt="minusIcon"/></td>
                   </tr>
                   </Fragment>
                 )
