c069c57bdacf6fce784cef09db715b41efc3112e
AustinPaley <austinpaley32@gmail.com>
2019-08-14 00:11:18 -0400

    bug fix for item stock saves in bag of holding

--- caravan-backend/app/controllers/bagofholdings_controller.rb
+++ caravan-backend/app/controllers/bagofholdings_controller.rb
@@ -11,8 +11,25 @@ class BagofholdingsController < ApplicationController
 
   def update
     @bagofholding = Bagofholding.find(params[:id])
-    newItems = params[:items].map do |item|
-      Item.find(item[:id])
+    newItems = []
+    params[:items].each do |item|
+      currentItem = Item.find(item[:id])
+      currentItem.update(
+        name: item[:name],
+        equipment_category: item[:equipment_category],
+        weapon_category: item[:weapon_category],
+        range: item[:range],
+        cost: item[:cost],
+        damage: item[:damage],
+        damage_type: item[:damage_type],
+        description: item[:description],
+        armor_category: item[:armor_category],
+        armor_class: item[:armor_class],
+        shop_id: item[:shop_id],
+        current_stock: item[:current_stock],
+        item_level: item[:item_level]
+      )
+      newItems.push(currentItem)
     end
     newSpells = params[:spells].map do |spell|
       Spell.find(spell[:id])
