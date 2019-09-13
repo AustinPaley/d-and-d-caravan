12e44ebfe9d80174e8e964faa09a75d3c865e2e3
AustinPaley <austinpaley32@gmail.com>
2019-09-13 03:08:25 -0400

    Fixed BoH Item Type Swap Save Bug

--- caravan-backend/app/controllers/items_controller.rb
+++ caravan-backend/app/controllers/items_controller.rb
@@ -30,25 +30,52 @@ class ItemsController < ApplicationController
   def update
     @bagofholding = Bagofholding.find(params[:bagofholding])
     if params[:item][:newItemClassification] === "item"
-      @item = @bagofholding.items.find(params[:id])
-      @item.update(
-        name: params[:item][:newItemName],
-        equipment_category: params[:item][:newItemType],
-        description: params[:item][:newItemDescription],
-        current_stock: params[:item][:newItemStock],
-        shop_id: 1
-      )
+      if @bagofholding.items.exists?(id: params[:id]) === true
+        @item = @bagofholding.items.find(params[:id])
+        @item.update(
+          name: params[:item][:newItemName],
+          equipment_category: params[:item][:newItemType],
+          description: params[:item][:newItemDescription],
+          current_stock: params[:item][:newItemStock],
+          shop_id: 1
+        )
+      elsif @bagofholding.items.exists?(id: params[:id]) === false
+        @olditem = @bagofholding.spells.find(params[:id])
+        @olditem.destroy()
+        newItem = Item.create(
+          name: params[:item][:newItemName],
+          equipment_category: params[:item][:newItemType],
+          description: params[:item][:newItemDescription],
+          current_stock: params[:item][:newItemStock],
+          shop_id: 1
+        )
+        @bagofholding.items.push(newItem)
+      end
+
     end
 
     if params[:item][:newItemClassification] === "spell"
-      @spell = @bagofholding.spells.find(params[:id])
-      @spell.update(
-        name: params[:item][:newItemName],
-        school: params[:item][:newItemType],
-        description: params[:item][:newItemDescription],
-        current_stock: params[:item][:newItemStock],
-        shop_id: 1
-      )
+      if @bagofholding.spells.exists?(id: params[:id]) === true
+        @spell = @bagofholding.spells.find(params[:id])
+        @spell.update(
+          name: params[:item][:newItemName],
+          school: params[:item][:newItemType],
+          description: params[:item][:newItemDescription],
+          current_stock: params[:item][:newItemStock],
+          shop_id: 1
+        )
+      elsif @bagofholding.spells.exists?(id: params[:id]) === false
+        @olditem = @bagofholding.items.find(params[:id])
+        @olditem.destroy()
+        newSpell = Spell.create(
+          name: params[:item][:newItemName],
+          school: params[:item][:newItemType],
+          description: params[:item][:newItemDescription],
+          current_stock: params[:item][:newItemStock],
+          shop_id: 1
+        )
+        @bagofholding.spells.push(newSpell)
+      end
     end
     render json: {bag: {id: @bagofholding.id, items: @bagofholding.items, spells: @bagofholding.spells, money: @bagofholding.money}}
   end
